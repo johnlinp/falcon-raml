62eaf718613a045e71079decb7137b0067cd14f5
johnlinp <johnlinp@gmail.com>
2017-01-03 14:02:01 +0800

    Fix setup.py for uploading to PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='falcon-raml',
-    version='0.1',
+    version='0.1.0',
     description='Parameter checker middleware using RAML for Falcon',
     author='johnlinp',
     author_email='johnlinp@gmail.com',
+    url='https://github.com/johnlinp/falcon-raml',
+    license='New BSD License',
     install_requires=[
         'falcon',
         'ramlfications',
